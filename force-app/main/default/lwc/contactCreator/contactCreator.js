4c0afb7d2462d877ab986dc1772b08a2ea538393
Carlos Pintor <36550078+carlos-pintor@users.noreply.github.com>
2021-11-29 23:07:59 -0600

    Add comments

--- force-app/main/default/lwc/contactCreator/contactCreator.js
+++ force-app/main/default/lwc/contactCreator/contactCreator.js
@@ -3,10 +3,16 @@ import createAccountContact from '@salesforce/apex/CAPS_ContactHelper.createAcco
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class ContactCreator extends LightningElement {
+  /**
+   * Se crean variables para almacenar los datos de los inputs
+   */
     firstName = "";
     lastName = "";
     companyName = "";
 
+    /**
+     * Se obtiene los valores ingresados en los inputs para guardarlos en cada variable
+     */
     handleChange(event) {
         const input = event.target;
         switch (input.name) {
@@ -24,28 +30,40 @@ export default class ContactCreator extends LightningElement {
         }
     }
 
+    /**
+     * LLama a saveRecord()
+     */
     handleSave() {
         this.saveRecord();
     }
 
+    /**
+     * Llama a clearFields()
+     */
     handleCancel() {
         this.clearFields();
     }
 
+    /**
+     * Llama a createAccountContact() y le envia los valores de las variables
+     */
     saveRecord() {
         console.log("saving record");
         createAccountContact({ firstName: this.firstName, lastName: this.lastName, company: this.companyName})
         .then((result) => {
             console.log("success");
-            this.showToast("success", "Saved successfuly", "");
+            this.showToast("success", "Saved successfuly", "");//Muestra mensaje de exito
             this.clearFields();
         })
         .catch((error) => {
             console.log("error", error);
-            this.showToast("error", "Error", "Error on saveRecord");
+            this.showToast("error", "Error", "Error on saveRecord");//Muestra mensaje de error
         });
     }
 
+    /**
+     * Limpia los valores de los inputs
+     */
     clearFields() {
         this.firstName = "";
         this.lastName = "";
@@ -55,6 +73,9 @@ export default class ContactCreator extends LightningElement {
         });
     }
 
+    /**
+     * Crea mensajes para mostrar, dependiendo de los parametros indicados
+     */
     showToast(variant, title, message) {
         const event = new ShowToastEvent({
             variant: variant,
